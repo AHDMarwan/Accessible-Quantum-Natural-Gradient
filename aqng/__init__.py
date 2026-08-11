e8ccddffd0646e087b36acf8db48694409fa13db
MaitHad <marwan.aithaddou@edu.uca.ac.ma>
2026-08-12 00:03:03 +0100

    Expose AQNG package version

--- aqng/__init__.py
+++ aqng/__init__.py
@@ -2,4 +2,6 @@
 
 from .standalone import AQNGOptimizer, ReadoutMode
 
-__all__ = ["AQNGOptimizer", "ReadoutMode"]
+__version__ = "0.6.0"
+
+__all__ = ["AQNGOptimizer", "ReadoutMode", "__version__"]
